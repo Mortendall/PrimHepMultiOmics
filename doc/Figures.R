8112c7aa365bb7db2127194801f5c4c8210b19f5
Morten Dall <mortendall1988@gmail.com>
2023-03-23 11:21:28 +0100

    Removed cpm-plots from figures

--- doc/Figures.R
+++ doc/Figures.R
@@ -5,25 +5,26 @@ cpm_figs <- targets::tar_read(cpmPlot)
 upsetGO <- targets::tar_read(UpsetplotGO)
 CompareClusterFigure_All <- targets::tar_read(GOCCdotplot)
 
+#decided to remove cpm-figs from main text as they were mainly for internal QC
+
 design_layout1 <- "
 1222
-3333
-4455
-4455
-##55
+3344
+3344
+##44
 "
 patchworktest <- pcaplotRNA +
     upsetRNA +
-    cpm_figs +
+    #cpm_figs +
     upsetGO+
     CompareClusterFigure_All+
     patchwork::plot_layout(design = design_layout1)+
     patchwork::plot_annotation(tag_levels = "A")&
     ggplot2::theme(plot.tag = ggplot2::element_text(size = 20))
 
- # grDevices::pdf(here::here("data/Figure1.pdf"), height = 20, width = 20)
- # patchworktest
- # dev.off()
+  grDevices::pdf(here::here("data/Figure1.pdf"), height = 20, width = 20)
+  patchworktest
+  dev.off()
 
 #####Figure 2####
 pcaplotProt <- targets::tar_read(MDSproteomics)
@@ -32,25 +33,26 @@ cpm_figsProt <- targets::tar_read(CPMplotProteomics)
 upsetGOProt <- targets::tar_read(UpsetplotGOProteomics)
 CompareClusterFigure_All_Prot <- targets::tar_read(DotplotCCProteomics)
 
+#decided to remove cpm-figs from main text as they were mainly for internal QC
+
 design_layout2 <- "
 1222
-3333
-4455
-4455
-##55
+3344
+3344
+##44
 "
 patchworktest2 <- pcaplotProt +
     upsetProt +
-    cpm_figsProt +
+    #cpm_figsProt +
     upsetGOProt+
     CompareClusterFigure_All_Prot+
     patchwork::plot_layout(design = design_layout2)+
     patchwork::plot_annotation(tag_levels = "A")&
     ggplot2::theme(plot.tag = ggplot2::element_text(size = 20))
 
-# grDevices::pdf(here::here("data/Figure2.pdf"), height = 20, width = 20)
-#  patchworktest2
-#  dev.off()
+ grDevices::pdf(here::here("data/Figure2.pdf"), height = 20, width = 20)
+  patchworktest2
+  dev.off()
 
 #####Figure 3####
 mdsPseudo <- targets::tar_read(PseudoMDS)
@@ -79,3 +81,20 @@ patchworktest3 <- mdsPseudo+
  # grDevices::pdf(here::here("data/Figure3.pdf"), height = 20, width = 20)
  # patchworktest3
  # dev.off()
+
+#####Figure 4-8####
+
+#as target is not well-suited to handle Seurat objects due to size,
+#figure 4-8 are generated in separate script. Code is available in
+#here::here("doc/SingleCellFigures.R")
+
+#####Patch figures together####
+qpdf::pdf_combine(input = c(here::here("data/Figure1.pdf"),
+                            here::here("data/Figure2.pdf"),
+                            here::here("data/Figure3.pdf"),
+                            here::here("data/Figure4.pdf"),
+                            here::here("data/Figure5.pdf"),
+                            here::here("data/Figure6.pdf"),
+                            here::here("data/Figure7.pdf"),
+                            here::here("data/Figure8.pdf")),
+                  output = here::here("data/Figures.pdf"))
--- doc/SingleCellFigures.R
+++ doc/SingleCellFigures.R
@@ -3,6 +3,7 @@
 #The UMAP plots are too large to save as target objects. Instead, they are
 #generated and saved through this script
 library(Brobdingnag)
+#restart R after running Brobdingnag
 library(tidyverse)
 library(Seurat)
 library(SummarizedExperiment)
@@ -488,3 +489,222 @@ DimplotCelltypePH <- Seurat::DimPlot(SeuratPH,
                                            size = 24),
         legend.position = "none"
     )
+
+#####Subset of endothelial cells####
+#  SeuratEndothelial <- subset(SingleCellData,
+#                              celltype == "Endothelial Cells"|
+#                                  seurat_clusters == 12)
+# SeuratEndothelial <- Seurat::RunPCA(SeuratEndothelial,
+#                             npcs=35,
+#                             seed.use=42,
+#                             verbose=F)
+# #runUMAP
+# SeuratEndothelial  <- Seurat::RunUMAP(SeuratEndothelial,
+#                               dims = 1:35,
+#                               seed.use = 43)
+# SeuratEndothelial  <- Seurat::FindNeighbors(SeuratEndothelial,
+#                                     reduction = "pca",
+#                                     dims = 1:35)
+#
+# SeuratEndothelial  <- Seurat::FindClusters(SeuratEndothelial ,
+#                                    random.seed = 42,
+#                                    resolution = 1.2,
+#                                    verbose = FALSE)
+
+ # saveRDS(SeuratEndothelial,
+ #         here::here("data/EndothelialSubset.rds"))
+
+ SeuratEndothelial<- readRDS(here::here("data/EndothelialSubset.rds"))
+ Idents(SeuratEndothelial)<- "Group"
+
+ DimplotEndothelial <- Seurat::DimPlot(SeuratEndothelial,
+                                        pt.size = 0.7,
+                                        label = T,
+                                        label.size = 6,
+                                        repel = T,
+                                        cols = wesanderson::wes_palette(3,
+                                                                        name = "Zissou1",
+                                                                        type = "continuous"),
+                                        label.box = T) +
+     ggplot2::ggtitle("DimPlot for Endothelial Cells") +
+     ggplot2::theme(
+         plot.title = ggplot2::element_text(hjust = 0.5,
+                                            size = 24),
+         legend.position = "none"
+     )
+DimplotEndothelial
+Idents(SeuratEndothelial)<- "seurat_clusters"
+# Endothelial_features <- Seurat::FindAllMarkers(SeuratEndothelial,
+#                                       only.pos = T,
+#                                       min.pct = 0.25,
+#                                       logfc.threshold = 0.25)
+#
+# saveRDS(Endothelial_features, here::here("data/EndothelialFeatures.rds"))
+
+Endothelial_features <- readRDS(here::here("data/EndothelialFeatures.rds"))
+
+markers_endofigure <- c("Arhgap31",
+                        "Adgrf5",
+                        "Gata4",
+                        "Itgb3",
+                        "Pkm",
+                        "Gclc",
+                        "Lipc")
+Idents(SeuratEndothelial)<- "Group"
+
+DotplotFigureEndo <- Seurat::DotPlot(SeuratEndothelial,
+                                 features = markers_endofigure)+
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                       vjust = 0.8,
+                                                       size = 18),
+                   axis.text.y = ggplot2::element_text(size = 18))
+DotplotFigureEndo
+
+#####Figure 7####
+design_layout7 <- "
+1122
+3344
+"
+patchworktest7 <-
+    DimplotCelltypePH+
+    DotplotFigurePH+
+    DimplotEndothelial+
+    DotplotFigureEndo+
+    patchwork::plot_layout(design = design_layout7)+
+    patchwork::plot_annotation(tag_levels = "A")&
+    ggplot2::theme(plot.tag = ggplot2::element_text(size = 20))
+
+  # grDevices::pdf(here::here("data/Figure7.pdf"), height = 20, width = 20)
+  # patchworktest7
+  # dev.off()
+
+#####Protein and RNA correlation####
+correlationData <- targets::tar_read(ProtRNACor)
+clusterCelltypeKey <- SingleCellData@meta.data |>
+    dplyr::select(celltype, seurat_clusters) |>
+    dplyr::distinct()
+annotatedMarkers <- dplyr::left_join(Markers,
+                                     clusterCelltypeKey,
+                                     by = c("cluster" = "seurat_clusters"))
+annotatedCor <- dplyr::left_join(correlationData,
+                                 annotatedMarkers,
+                                 by = "gene") |>
+    dplyr::select(gene, log2FoldChange, logFC, celltype) |>
+    dplyr::distinct()
+
+AnnotatedComparisonPlot <- ggplot2::ggplot(annotatedCor,
+                                           ggplot2::aes(x = log2FoldChange,
+                                                        y = logFC))+
+    ggplot2::geom_point(size = 2)+
+    ggplot2::theme_bw()+
+    ggplot2::xlab("Pseudobulk RNA exp. Log2FC")+
+    ggplot2::ylab("Protein Ab. Log2FC")+
+    ggplot2::ggtitle("Liver vs PH",
+                     "Protein abundance vs. RNA expression")+
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5,
+                                                      size = 24),
+                   plot.subtitle = ggplot2::element_text(hjust = 0.5,
+                                                         size = 22),
+                   axis.title = ggplot2::element_text(size = 20),
+                   axis.text = ggplot2::element_text(size = 20))+
+    ggplot2::facet_wrap(~celltype)
+
+#select candidates with either high or low abundance in cultured hepatocytes
+candidates <- annotatedCor|>
+    dplyr::filter(logFC>2|logFC< -2) |>
+    dplyr::arrange(logFC)
+
+#make heatmap of candidates
+proteomicsData <- targets::tar_read(NormalizedMatrix)
+nameConverter <- clusterProfiler::bitr(rownames(proteomicsData),
+                                       fromType = "ACCNUM",
+                                       toType = "SYMBOL",
+                                       OrgDb = "org.Mm.eg.db")
+proteomicsDataAnno <- proteomicsData |>
+    as.data.frame() |>
+    dplyr::mutate(ACCID = rownames(proteomicsData))
+proteomicsDataAnno <- dplyr::left_join(proteomicsDataAnno,
+                                       nameConverter,
+                                       by = c("ACCID"="ACCNUM")) |>
+    dplyr::filter(!is.na(SYMBOL)) |>
+    dplyr::distinct(SYMBOL, .keep_all = T)
+
+proteomicsDataAnno <- proteomicsDataAnno|>
+    magrittr::set_rownames(proteomicsDataAnno$SYMBOL) |>
+    dplyr::select(-SYMBOL,
+                  -ACCID)
+
+#Select Candidates in Gene List
+trimmed_cpm <- dplyr::filter(proteomicsDataAnno,
+                             rownames(proteomicsDataAnno) %in% candidates$gene)
+
+#create annotation key for heatmap
+setup <- targets::tar_read(ProteomicsSetup)
+key <- as.data.frame(setup)
+key <- key |>
+    dplyr::select(Tissue)
+rownames(key) <- setup$SampleID
+key$Tissue <- factor(key$Tissue, c("liver", "CS", "PH"))
+
+#create heatmap
+HeatmapProteome <- pheatmap::pheatmap(trimmed_cpm,
+                              treeheight_col = 0,
+                              treeheight_row = 0,
+                              scale = "row",
+                              legend = T,
+                              na_col = "white",
+                              Colv = NA,
+                              na.rm = T,
+                              cluster_cols = F,
+                              fontsize_row = 12,
+                              fontsize_col = 12,
+                              cellwidth = 16,
+                              cellheight = 12,
+                              annotation_col = key,
+                              show_colnames = F,
+                              show_rownames = T,
+                              main = "Candidates from proteomics data",
+                              cluster_rows = T
+)
+HeatmapProteome <- ggplotify::as.ggplot(HeatmapProteome,scale = 1.5)
+
+glut1Plot <- Seurat::FeaturePlot(SingleCellData,
+                         features = "Slc2a1",
+                         pt.size = 1)+
+    ggplot2::ggtitle("Glut1 Expression\n Cultured Hepatocytes")+
+    ggplot2::theme(title = ggplot2::element_text(size = 22))
+
+glut2Plot <- Seurat::FeaturePlot(SingleCellData,
+                                 features = "Slc2a2",
+                                 pt.size = 1)+
+    ggplot2::ggtitle("Glut2 Expression\n Freshly Isolated Hepatocytes")+
+    ggplot2::theme(title = ggplot2::element_text(size = 22))
+
+Idents(SingleCellData)<-"celltype"
+DotplotCandidateFigure <- Seurat::DotPlot(SingleCellData,
+                                 features = rownames(trimmed_cpm))+
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                       vjust = 0.8,
+                                                       size = 18),
+                   axis.text.y = ggplot2::element_text(size = 18)
+                   )+
+    ggplot2::coord_flip()
+
+
+#####Figure 8####
+design_layout8 <- "
+1122
+3344
+"
+patchworktest8 <-
+    HeatmapProteome+
+    DotplotCandidateFigure+
+    glut1Plot+
+    glut2Plot+
+    patchwork::plot_layout(design = design_layout8)+
+    patchwork::plot_annotation(tag_levels = "A")&
+    ggplot2::theme(plot.tag = ggplot2::element_text(size = 20))
+
+  # grDevices::pdf(here::here("data/Figure8.pdf"), height = 20, width = 20)
+  # patchworktest8
+  # dev.off()
